3e37b11cb29fae7be6451bcc617ebfd95aff691a
wdfeer <wdf-is-git@tutanota.de>
2022-04-11 15:42:22 +0200

    Fetch grades

--- main.sh
+++ main.sh
@@ -1,7 +1,9 @@
-curl --cookie-jar cjar --output log/one.html https://nz.ua/
+curl --cookie-jar cjar \
+	--output log/one.html \
+	"https://nz.ua/"
 login=`cat env/login.txt`
 password=`cat env/password.txt`
-echo "Enter the CSRF:"
+echo "Enter the CSRF, obtained from log/one.html:"
 read csrf
 curl --cookie cjar --cookie-jar cjar \
 	--output log/two.html \
@@ -10,5 +12,8 @@ curl --cookie cjar --cookie-jar cjar \
 	--data "LoginForm[login]=$login" \
 	--data "LoginForm[password]=$password" \
 	--data "LoginForm[rememberMe]=1" \
-	https://nz.ua/
+	"https://nz.ua/"+
+curl --cookie cjar --cookie-jar cjar \
+	--output log/grades.html \
+	"https://nz.ua/schedule/grades-statement"
 mv cjar log/cjar
